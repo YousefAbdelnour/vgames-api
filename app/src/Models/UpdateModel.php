55424c36c2e72b5ffe9c315d46ce5c95db670b6f
YousefAbdelnour <112569045+YousefAbdelnour@users.noreply.github.com>
2024-09-30 19:06:44 -0400

    Implemented filters

--- app/src/Models/UpdateModel.php
+++ app/src/Models/UpdateModel.php
@@ -18,6 +18,41 @@ class UpdateModel extends BaseModel
         $query_args = [];
 
         $sql = "SELECT * FROM {$this->table_name} WHERE 1 ";
+        if (isset($params["Limited_Time_Event"])) {
+            //* TODO: parse 0 and 1 to true and false
+            $sql .= " AND Limited_Time_Event = :Limited_Time_Event ";
+            $query_args['Limited_Time_Event'] = $params['Limited_Time_Event'];
+        }
+        if (isset($params['Until_Date']) && isset($params['From_Date'])) {
+            $sql .= ' AND Date BETWEEN :Until_Date AND :From_Date ';
+            $query_args['Until_Date'] = $params['Until_Date'];
+            $query_args['From_Date'] = $params['From_Date'];
+        } else if (isset($params['Until_Date'])) {
+            $sql .= ' AND Date <= :Until_Date';
+            $query_args['Until_Date'] = $params['Until_Date'];
+        } else if (isset($params['From_Date'])) {
+            $sql .= ' AND Date >= :From_Date';
+            $query_args['From_Date'] = $params['From_Date'];
+        }
+        if (isset($params['Min_Update_Size']) && isset($params['Max_Update_Size'])) {
+            $sql .= ' AND Update_Size BETWEEN :Min_Update_Size AND :Max_Update_Size ';
+            $query_args['Min_Update_Size'] = $params['Min_Update_Size'];
+            $query_args['Max_Update_Size'] = $params['Max_Update_Size'];
+        } else if (isset($params['Max_Update_Size'])) {
+            $sql .= ' AND Update_Size <= :Max_Update_Size';
+            $query_args['Max_Update_Size'] = $params['Max_Update_Size'];
+        } else if (isset($params['Min_Update_Size'])) {
+            $sql .= ' AND Update_Size >= :Min_Update_Size';
+            $query_args['Min_Update_Size'] = $params['Min_Update_Size'];
+        }
+        if (isset($params['Description'])) {
+            $sql .= ' AND Description LIKE :Description';
+            $query_args['Description'] = '%' . $params['Description'] . '%';
+        }
+        if (isset($params['Version_Number'])) {
+            $sql .= ' AND Version_Number LIKE CONCAT(:Version_Number, "%")';
+            $query_args['Version_Number'] = $params['Version_Number'];
+        }
         $result = $this->paginate($sql, $query_args);
 
         //parsing Features
@@ -32,13 +67,14 @@ class UpdateModel extends BaseModel
         $result = $this->fetchSingle($sql, ["update_id" => $update_id]);
 
         //* Parsing the New Features Column
-        $result['New_Features'] = explode(',',$result["New_Features"]);
+        $result['New_Features'] = explode(',', $result["New_Features"]);
         return $result;
     }
 
-    private function parseNewFeatures($data){
-        foreach ($data as &$row){
-            $row['New_Features'] = explode(',',$row["New_Features"]);
+    private function parseNewFeatures($data)
+    {
+        foreach ($data as &$row) {
+            $row['New_Features'] = explode(',', $row["New_Features"]);
         }
         return $data;
     }
