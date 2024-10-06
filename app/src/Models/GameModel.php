b025fb303605ef3ee0d1714633d6da35a9521d47
Denis Voronov <vorden2005@gmail.com>
2024-10-06 11:41:13 -0400

    Filter and sort GET /games

--- app/src/Models/GameModel.php
+++ app/src/Models/GameModel.php
@@ -8,6 +8,9 @@ class GameModel extends BaseModel
 {
 
     private string $table_name = "game";
+    public array $fields = ['game_id', 'developer_id', 'genre_name', 'name', 'founder', 'release_date', 'country_name', 'esrb', 'price', 'number_of_players', 'avg_rating'];
+
+    public string $default_sort_field = 'game_id';
 
     public function __construct(PDOService $pdo, private CountryModel $country_model, private DeveloperModel $developer_model, private GenreModel $genre_model)
     {
@@ -18,11 +21,80 @@ class GameModel extends BaseModel
     {
         $query_args = [];
 
-        //TODO: filtering, sorting
+        $sql = <<<SQL
+        SELECT g.*, IFNULL(ROUND(AVG(r.rating), 1), 0) AS avg_rating
+            FROM {$this->table_name} g
+                LEFT JOIN review r
+                    ON g.game_id = r.game_id
+                        GROUP BY g.game_id, g.name
+                            HAVING 1
+        SQL;
 
-        $sql = "SELECT * FROM {$this->table_name} WHERE 1 ";
+        $this->filter($params, $sql, $query_args);
+        $this->sort($params, $sql, $this);
 
-        return (array) $this->paginate($sql, $query_args);
+        $games =  (array) $this->paginate($sql, $query_args);
+
+        return $games;
+    }
+
+    private function filter(array $params, String &$sql, array &$query_args)
+    {
+        // GENRE
+        if (isset($params["genre_name"])) {
+            $sql .= " AND genre_name = :genre_name ";
+            $query_args['genre_name'] = $params['genre_name'];
+        }
+
+        //  RELEASE DATE
+        if (isset($params['from_release_date']) && isset($params['to_release_date'])) {
+            $sql .= ' AND release_date BETWEEN :from_release_date AND :to_release_date ';
+            $query_args['from_release_date'] = $params['from_release_date'];
+            $query_args['to_release_date'] = $params['to_release_date'];
+        } else if (isset($params['to_release_date'])) {
+            $sql .= ' AND release_date <= :to_release_date';
+            $query_args['to_release_date'] = $params['to_release_date'];
+        } else if (isset($params['from_release_date'])) {
+            $sql .= ' AND release_date >= :from_release_date';
+            $query_args['from_release_date'] = $params['from_release_date'];
+        } else if (isset($params['release_date'])) {
+            $sql .= ' AND release_date = :release_date';
+            $query_args['release_date'] = $params['release_date'];
+        }
+
+        // NAME
+        if (isset($params['name'])) {
+            $sql .= ' AND name LIKE :name';
+            $query_args['name'] = '%' . $params['name'] . '%';
+        }
+
+        // COUNTRY NAME
+        if (isset($params['country_name'])) {
+            $sql .= ' AND country_name LIKE :country_name';
+            $query_args['country_name'] = '%' . $params['country_name'] . '%';
+        }
+
+        // FOUNDER
+        if (isset($params['founder'])) {
+            $sql .= ' AND founder LIKE :founder';
+            $query_args['founder'] = '%' . $params['founder'] . '%';
+        }
+
+        // AVERAGE RATING
+        if (isset($params["min_avg_rating"]) && isset($params["max_avg_rating"])) {
+            $sql .= " AND avg_rating BETWEEN :min_avg_rating AND :max_avg_rating ";
+            $query_args['min_avg_rating'] = $params['min_avg_rating'];
+            $query_args['max_avg_rating'] = $params['max_avg_rating'];
+        } else if (isset($params["min_avg_rating"])) {
+            $sql .= " AND avg_rating >= :min_avg_rating";
+            $query_args['min_avg_rating'] = $params['min_avg_rating'];
+        } else if (isset($params["max_avg_rating"])) {
+            $sql .= " AND avg_rating <= :max_avg_rating";
+            $query_args['max_avg_rating'] = $params['max_avg_rating'];
+        } else if (isset($params['avg_rating'])) {
+            $sql .= " AND avg_rating = :avg_rating";
+            $query_args['avg_rating'] = $params['avg_rating'];
+        }
     }
 
     public function getGameById($game_id): mixed
