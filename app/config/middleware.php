965a5b5b4d649d08a5b2db2d2cb3a9e665e5d826
RoLajoie <rowanlajoie@hotmail.com>
2024-11-22 10:40:53 -0500

    Added key generation using the login route temporarily

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 use App\Core\CustomErrorHandler;
 use App\Middleware\ContentNegotiationMiddleware;
 use App\Middleware\LoggerMiddleware;
+use App\Middleware\AccountMiddleware;
 use Slim\App;
 use App\Helpers\LogHelper as Logger;
 
 return function (App $app) {
     // Add your middleware here.
+    $app->addMiddleware(new AccountMiddleware());
     $app->addMiddleware(new ContentNegotiationMiddleware());
     $app->addBodyParsingMiddleware();
     $app->addRoutingMiddleware();
