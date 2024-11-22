f6ddba78a9a8d94c5ac5455e3c799683b5ee6da1
Yousef Abdelnour <2242238@edu.vaniercollege.qc.ca>
2024-11-22 10:11:46 -0500

    fix

--- app/src/Controllers/AccountController.php
+++ app/src/Controllers/AccountController.php
@@ -17,7 +17,7 @@ class AccountController extends BaseController
     {
         // user_id first_name last_email email password role created_at
         $new_user = $request->getParsedBody();
-        $result = $this->accountsService->createUpdate($new_user);
+        $result = $this->accountsService->createAccount($new_user);
         $status = $result->isSuccess() ? HTTP_CREATED : 400;
 
         if ($result->isSuccess()) {
